29bb38ae5a722055b78a4f90385160c593e9336b
Levi Borland <borland.levi@gmail.com>
2019-07-13 08:47:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  ["Green Bay Packers", "Chicago Bears", "Minnesota Vikings", "Atlanta Falcons"]
 end
 
 def add_element_to_end_of_array(array, element)
